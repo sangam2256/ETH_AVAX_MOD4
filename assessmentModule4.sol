fefc9128d870b7d16e23f7930db75ae0ccf8d120
Sangam Kumar <132322216+sangam2256@users.noreply.github.com>
2024-09-07 10:19:30 +0530

    Update assessmentModule4.sol

--- assessmentModule4.sol
+++ assessmentModule4.sol
@@ -1,52 +1,62 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-//From starter template project  
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DegenToken is ERC20, ERC20Burnable, Ownable{
-     string[] public arr;
+contract DegenToken is ERC20, Ownable {
+    mapping(address => string[]) private _purchases;
+    mapping(string => uint256) private _clothPrices;
 
-    constructor() ERC20( "Degen",
-                           "DGN")
-        { 
-        
+    event Burn(address indexed from, uint256 value);
+    event Redeem(address indexed from, uint256 value, string itemName);
 
-        arr = new string[](3);
-       
-        arr = ["Position 1", "Position 2", "Position 3"];
-        }
-
-    function redeem(uint256 _position,uint256 __price) public payable  {
-          require(
-            bytes(arr[_position]).length > 0,
-            "Position does not exist"
-        );
-
-        require(
-            balanceOf(msg.sender) > 0,
-            "Insufficient balance"
-        );
-        _burn(_msgSender(),__price);
-    }
-    
- function transfer(address _beneficiery,uint256 __price) public override returns (bool success){
-        require(__price > 0,"_price should be greater than zero.");
-        _transfer(_msgSender(),_beneficiery,__price);
-        return true;
-    }
-    
-
-    function mint(address _beneficiery, uint256 __price) external onlyOwner{
-        _mint(_beneficiery,__price);
+    constructor() ERC20("Degen", "DGN") Ownable(msg.sender) {
+        // Initialize item prices
+        _clothPrices["SHIRTS"] = 500;
+        _clothPrices["T-SHIRTS"] = 400;
+        _clothPrices["SKIRT"] = 250;
+        _clothPrices["CAP"] = 150;
+        _clothPrices["FROCK"] = 600;
     }
 
-    
+    function decimals() public view virtual override returns (uint8) {
+        return 0;
+    }
 
-    function checkBalance(address account) external view returns (uint256){
-        return balanceOf(account);
+    function mint(address to, uint256 value) public onlyOwner {
+        _mint(to, value);
+    }
+
+    function transfer(address to, uint256 value) public override returns (bool) {
+        return super.transfer(to, value);
+    }
+
+    function redeem(string memory itemName) public {
+        address sender = msg.sender;
+        uint256 itemPrice = _clothPrices[itemName];
+        require(itemPrice > 0, "Item does not exist");
+        require(balanceOf(sender) >= itemPrice, "Insufficient balance");
+
+        _burn(sender, itemPrice);
+        _purchases[sender].push(itemName);
+        emit Redeem(sender, itemPrice, itemName);
+    }
+
+    function burn(uint256 value) public {
+        _burn(msg.sender, value);
+        emit Burn(msg.sender, value);
+    }
+
+    function getPurchases(address account) public view returns (string[] memory) {
+        return _purchases[account];
+    }
+
+    function getItemPrice(string memory itemName) public view returns (uint256) {
+        return _clothPrices[itemName];
+    }
+
+    function setItemPrice(string memory itemName, uint256 price) public onlyOwner {
+        _clothPrices[itemName] = price;
     }
 }
